1098ecc139c703e3c9e913a50a69d88c2b42483f
rushomrong <rushomarak@gmail.com>
2025-05-04 16:44:36 +0600

    Building employee management system using react

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div className='max-w-9/12 text-center'>
+      <h1 className='w-auto text-4xl text-black'>Employee Management System - Rusho</h1>
+      </div>
+    
+    </>
+  )
+}
+
+export default App
