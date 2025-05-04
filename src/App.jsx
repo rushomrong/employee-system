3757efd57d9e3a95818b005e02226da8c8015ffc
rushomrong <rushomarak@gmail.com>
2025-05-04 16:54:25 +0600

    added react router, admin, employee and login pages without context

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
+import { Router, Routes } from 'react-router-dom'
 import './App.css'
 
 function App() {
 
 
   return (
-    <>
-      <div className='max-w-9/12 text-center'>
-      <h1 className='w-auto text-4xl text-black'>Employee Management System - Rusho</h1>
-      </div>
-    
-    </>
+   
+   <Router>
+    <Routes>
+      
+    </Routes>
+   </Router>
   )
 }
 
